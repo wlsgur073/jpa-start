77886eeda075c9afa1704e38ee87bc0f3bf39b2c
wlsgur073 <wlsgur073@gmail.com>
2024-09-24 11:29:17 +0900

    bulk 연산 주의사항 commit

--- src/main/java/jpql/JpaSql.java
+++ src/main/java/jpql/JpaSql.java
@@ -43,6 +43,21 @@ public class JpaSql {
 
             /* 결론 QueryDSL을 사용하자. */
 
+            /*
+                executeUpdate()의 경우 auto flush 기능이 있기에 DB에는 자동 반영된다.
+                그러나 영속성 컨텍스트에서는 반영되어 있지 않는다. 왜냐하면 아직 clear가 되어 있지 않기 때문이다.
+                이러한 현상은 DB와 영속성 컨텍스트의 데이터 정합성에 맞지 않는 현상이 발생할 수 있다.
+                그러기에 em.clear()를 해서 영속성 컨텍스트를 초기화 시킨 후, DB에서 다시 조회해서
+                영속성 컨텍스트를 DB에 맞춰야 한다.
+
+
+                https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.modifying-queries
+                위에 Spring data JPA docs를 참고하면 "we do not automatically clear it"이라 작성되어 있다.
+            */
+//            em.createQuery("update Member m set m.name = 'kim'").executeUpdate();
+//            em.clear();
+//            em.find(Member.class, member1.getId());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
